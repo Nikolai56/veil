c08fe4cffa93d1ff434a7e6ab5ce11a9ebdf63d1
Nikolai56 <nikolay@Nikolays-Air.Dlink>
2018-11-28 22:38:19 +0300

    added Owl Carousel

--- js/main.js
+++ js/main.js
@@ -3,4 +3,25 @@ jQuery(document).ready(function($) {
     event.preventDefault();
     $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
   });
+
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+      0:{
+        items:1,
+        nav:true
+      },
+      600:{
+        items:3,
+        nav:false
+      },
+      1000:{
+        items:5,
+        nav:true,
+        loop:false
+      }
+    }
+  })
 });
