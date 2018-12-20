cb279907d8fef429ce556fced448c7f38edaf93c
Nikolai56 <nikolay@Nikolays-Air.Dlink>
2018-12-20 10:01:22 +0300

    added controls to carousels

--- js/main.js
+++ js/main.js
@@ -8,8 +8,8 @@ jQuery(document).ready(function($) {
   var owl1 = $('.owl-projects');
   owl1.owlCarousel({
     loop:true,
-    nav: false,
-    dots: false,
+    nav:false,
+    dots:false,
     margin:50,
     responsiveClass:true,
     responsive:{
@@ -21,35 +21,77 @@ jQuery(document).ready(function($) {
       },
       1000:{
         items:4,
-        loop:false
       }
     }
   });
 
 // Go to the next item
-  $('.ourProjects .arrow-right').after().click(function() {
+  $('.ourProjects .arrow-right').click(function() {
     owl1.trigger('next.owl.carousel');
   });
 // Go to the previous item
-  $('.ourProjects .arrow-left').before().click(function() {
+  $('.ourProjects .arrow-left').click(function() {
     owl1.trigger('prev.owl.carousel');
   });
 
 
-  $('.owl-videos').owlCarousel({
+  var owl2 = $('.owl-videos');
+  owl2.owlCarousel({
     loop:true,
+    nav:false,
+    dots:false,
     margin:10,
     responsiveClass:true,
     responsive:{
       0:{
         items:1,
-        nav:true
       },
       600:{
         items:3,
-        nav:false
       }
     }
   });
+
+  // Go to the next item
+  $('.videos .arrow-right').click(function() {
+    owl2.trigger('next.owl.carousel');
+  });
+// Go to the previous item
+  $('.videos .arrow-left').click(function() {
+    owl2.trigger('prev.owl.carousel');
+  });
+
+
+  var owl3 = $('.owl-diplomas');
+  owl3.owlCarousel({
+    loop:true,
+    nav:false,
+    dots:false,
+    margin:38,
+    responsiveClass:true,
+    responsive:{
+      0:{
+        items:1,
+      },
+      600:{
+        items:3,
+      },
+      1000:{
+        items:4,
+      }
+    }
+  });
+
+// Go to the next item
+  $('.diplomas .arrow-right').click(function() {
+    owl3.trigger('next.owl.carousel');
+  });
+// Go to the previous item
+  $('.diplomas .arrow-left').click(function() {
+    owl3.trigger('prev.owl.carousel');
+  });
+
+
+
 
 });
