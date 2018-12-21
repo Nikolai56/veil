b92e49fb654436a0563ce0a3936ffdd182bf736f
Nikolai56 <nikolay@Nikolays-Air.Dlink>
2018-12-21 21:14:34 +0300

    updated navigation

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 jQuery(document).ready(function($) {
   $(".scroll").click(function(event){
     event.preventDefault();
-    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+    if (window.innerWidth < 768) {
+      $('.navbar-toggler').click();
+    }
+    var height = $('.sticky-top')[0].clientHeight - 1;
+    $('html,body').animate({scrollTop:($(this.hash).offset().top) - height}, 500);
   });
 
 
@@ -89,6 +93,25 @@ jQuery(document).ready(function($) {
 // Go to the previous item
   $('.diplomas .arrow-left').click(function() {
     owl3.trigger('prev.owl.carousel');
+  });
+
+
+  var owl4 = $('.owl-main');
+  owl4.owlCarousel({
+    loop:true,
+    nav:false,
+    dots:false,
+    margin:0,
+    items: 1,
+  });
+
+  // Go to the next item
+  $('.main-carousel .arrow-right').click(function() {
+    owl4.trigger('next.owl.carousel');
+  });
+// Go to the previous item
+  $('.main-carousel .arrow-left').click(function() {
+    owl4.trigger('prev.owl.carousel');
   });
 
 
