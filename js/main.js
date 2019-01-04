37fb4e7f0126965201c16d598246000be36fdaf9
Nikolai56 <nikolay@Nikolays-Air.Dlink>
2019-01-05 01:47:57 +0300

    added modals

--- js/main.js
+++ js/main.js
@@ -115,6 +115,16 @@ jQuery(document).ready(function($) {
   });
 
 
+  $('#writeToUs').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget); // Button that triggered the modal
+    var title = button.data('title'); // Extract info from data-* attributes
+    var hidden = button.data('hidden') || title; // Extract info from data-* attributes
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    var modal = $(this);
+    modal.find('.modal-title').text(title);
+    modal.find('.input-hidden-title').val(hidden);
+  })
 
 
 });
