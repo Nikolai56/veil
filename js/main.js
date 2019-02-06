94c6234ab41c71c8b969a750a3d9e1c4cd77d1a7
Nikolai56 <nikolay@Nikolays-Air.Dlink>
2019-02-06 23:19:26 +0300

    added gallery to ourProjects section

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ jQuery(document).ready(function($) {
 
   var owl1 = $('.owl-projects');
   owl1.owlCarousel({
-    loop:true,
+    loop:false,
     nav:false,
     dots:false,
     margin:50,
@@ -130,6 +130,32 @@ jQuery(document).ready(function($) {
 
   $(document).on('submit', '.ajax_form', function() {
     $('#writeToUs').modal('hide');
+  });
+
+  $('.gallery').each(function() { // the containers for all your galleries
+    $(this).magnificPopup({
+      delegate: 'a', // the selector for gallery item
+      type: 'image',
+      gallery: {
+        enabled:true
+      },
+      mainClass: 'mfp-with-zoom',
+      zoom: {
+        enabled: true, // By default it's false, so don't forget to enable it
+
+        duration: 300, // duration of the effect, in milliseconds
+        easing: 'ease-in-out', // CSS transition easing function
+
+        // The "opener" function should return the element from which popup will be zoomed in
+        // and to which popup will be scaled down
+        // By defailt it looks for an image tag:
+        opener: function(openerElement) {
+          // openerElement is the element on which popup was initialized, in this case its <a> tag
+          // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+          return openerElement.is('img') ? openerElement : openerElement.find('img');
+        }
+      }
+    });
   });
 
 });
